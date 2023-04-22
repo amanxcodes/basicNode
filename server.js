c005ccad30d0dfc2ca6599a34a47061d25e3e0c8
amanpreetsingh214 <53363558+amanpreetsingh214@users.noreply.github.com>
2023-04-22 18:30:29 -0400

    Little changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.post('/',(req,res)=>{
     const num2= Number(req.body.num2);
     const sum= num1+num2;
     res.send(`The sum of ${num1} and ${num2} is ${sum}`);
-    console.log(`Result from post is ${sum}`);
+    console.log(`Result from post is ${sum}.`);
 })
 
 app.listen(3000,()=>{
